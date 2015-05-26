64d45036357299de74238e02c18027c90bfd64f3
Olivier Raggi <olivier@raggi.fr>
2015-05-26 22:26:49 +0200

    Loading YAML configuration file test

--- views/_header.tpl
+++ views/_header.tpl
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title></title>
+    <title>{{name}} - {{site.title}}</title>
 
     <!-- Bootstrap -->
     <!--
--- vikio.py
+++ vikio.py
@@ -20,6 +20,11 @@ parser = CommonMark.DocParser()
 renderer = CommonMark.HTMLRenderer()
 
 
+class Struct:
+    def __init__(self, **entries): 
+        self.__dict__.update(entries)
+
+
 def config(config_path = 'config.yml'):
     """Get configuration parameters
     Get data from YAML configuration file
@@ -31,15 +36,15 @@ def config(config_path = 'config.yml'):
         vikio_config = yaml.load(file(config_path, 'r'))
     except yaml.YAMLError, exc:
         print('Error in configuration file: %s', exc)
-
-    #print(yaml.dump(vikio_config))
-    return vikio_config
+ 
+    site = Struct(**vikio_config['site'])
+    return site
 
 
 @error(404)
 @view('error')
 def error404(error):
-    return dict(error=error)
+    return dict(error=error, name='error404', site=site)
 
 
 @route('/css/<filename:path>')
@@ -63,21 +68,25 @@ def download(filename):
 
 
 @route('/')
-def index(name='Stranger'):
+def index():
     redirect("/page/index")
 
 
+@route('/page')
+@route('/page/')
 @route('/page/<name>')
 @view('default')
 def hello(name='index'):
     document = codecs.open("pages/{}.md".format(name), 'r', encoding='utf-8')
     ast = parser.parse(document.read())
     html = renderer.render(ast)
-    return dict(content=html, name=name)
+    return dict(content=html, name=name, site=site)
 
 
 def main():
     """ Run the whole program """
+    global site
+    site = config()
     run(host='localhost', port=8080, debug=True, reloader=True)
 
 
